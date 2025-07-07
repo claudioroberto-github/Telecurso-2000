710a41160c6185c4435890e9e7c21aaa4886a641
claudioroberto-github <claudiorobertocordeirorui@gmail.com>
2025-07-07 15:53:14 -0300

    ajeitei algumas coisas do menu.php

--- menu.php
+++ menu.php
@@ -19,7 +19,7 @@ $logged = $_SESSION['user'];
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Sidebar Menu | CodingNepal</title>
   <link rel="stylesheet" href="assets/css/home/home.css " />
-  <link rel="stylesheet" href="test/js/styles-loja.css" />
+  <link rel="stylesheet" href="assets/css/menu/styles-loja.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
 </head>
 
